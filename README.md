4d6dc7bee90de343dccf89ad75e7afd113a5cd94
syuchan1005 <syuchan.dev@gmail.com>
2016-12-31 02:33:31 +0900

    ライセンス表記を追加

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # YomiageKun
-   You need to replace `resources/APIKey.properties.`
+   You need to replace `src/main/java/resources/APIKey.properties.`
+```text
+DocomoSpeechToText = <Your APIKey Here!>
+```
+
+## License
+Released under the MIT license.
+Please see `src/main/java/resources/LICENSE`
---
+++ src/main/java/com/github/syuchan1005/yomiagekun/controllers/LicenseController.java
@@ -0,0 +1,51 @@
+package com.github.syuchan1005.yomiagekun.controllers;
+
+import com.github.syuchan1005.yomiagekun.Controller;
+import com.github.syuchan1005.yomiagekun.Util;
+import com.github.syuchan1005.yomiagekun.contents.LicenseContent;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by syuchan on 2016/12/31.
+ */
+public class LicenseController extends Controller implements Initializable {
+	@FXML
+	private TextArea thisApplication;
+	@FXML
+	private TableView tableView;
+	@FXML
+	private TableColumn libraryName;
+	@FXML
+	private TableColumn license;
+	@FXML
+	private TextArea apacheLicense2;
+
+	private ObservableList<LicenseContent> licenseContents = FXCollections.observableArrayList();
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		thisApplication.setText(Util.convertString(ClassLoader.getSystemResourceAsStream("LICENSE")));
+		libraryName.setCellValueFactory(new PropertyValueFactory<LicenseContent, String>("libraryName"));
+		license.setCellValueFactory(new PropertyValueFactory<LicenseContent, String>("license"));
+		tableView.setItems(licenseContents);
+		licenseContents.add(new LicenseContent("Docomo Aitalk", "Apache License 2.0"));
+		licenseContents.add(new LicenseContent("commons-codec", "Apache License 2.0"));
+		licenseContents.add(new LicenseContent("commons-logging", "Apache License 2.0"));
+		licenseContents.add(new LicenseContent("httpcore", "Apache License 2.0"));
+		licenseContents.add(new LicenseContent("httpclient", "Apache License 2.0"));
+		licenseContents.add(new LicenseContent("httpmime", "Apache License 2.0"));
+		licenseContents.add(new LicenseContent("jackson", "Apache License 2.0"));
+		licenseContents.add(new LicenseContent("logback", "LGPL 2.1 & EPL 1.0"));
+		apacheLicense2.setText(Util.convertString(ClassLoader.getSystemResourceAsStream("ApacheLicense2")));
+	}
+}
